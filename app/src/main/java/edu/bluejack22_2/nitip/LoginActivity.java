618519b6e726a9584577538b649c65aba14fa757
Kevin Christian <kdotchrist30@gmail.com>
2023-04-06 17:08:04 +0700

    Started Log In Page

---
+++ app/src/main/java/edu/bluejack22_2/nitip/LoginActivity.java
@@ -0,0 +1,14 @@
+package edu.bluejack22_2.nitip;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
