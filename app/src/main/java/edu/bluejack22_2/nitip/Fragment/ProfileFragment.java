802391e6162bf26331c06fcbf77b1e6faa341e8a
Kevin Christian <kdotchrist30@gmail.com>
2023-04-20 16:17:10 +0700

    Added Profile Page UI, Fixed Dark Theme Coloring

--- app/src/main/java/edu/bluejack22_2/nitip/Fragment/ProfileFragment.java
+++ app/src/main/java/edu/bluejack22_2/nitip/Fragment/ProfileFragment.java
@@ -52,53 +52,52 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_profile, container, false);
 
-        initialize(view);
-        setUser();
-        setListener(view);
+//        initialize(view);
+//        setUser();
+//        setListener(view);
 
         return view;
     }
 
-    private void setUser() {
-        userViewModel.getUser(firebaseUser.getEmail()).observe(getViewLifecycleOwner(), user -> {
-            if (user != null) {
-                this.user = new User(user.getUsername(), user.getEmail(), "", user.getProfile());
-                setProfilePicture(view);
-            }
-        });
-    }
+//    private void setUser() {
+//        userViewModel.getUser(firebaseUser.getEmail()).observe(getViewLifecycleOwner(), user -> {
+//            if (user != null) {
+//                this.user = new User(user.getUsername(), user.getEmail(), "", user.getProfile());
+//                setProfilePicture(view);
+//            }
+//        });
+//    }
 
-    private void initialize(View view) {
-        btnLogout = view.findViewById(R.id.btnLogout);
-        ivProfile = view.findViewById(R.id.ivProfile);
-        gso = GoogleService.getGso(view.getContext());
-        gsc = GoogleService.getGsc(view.getContext());
-        loginViewModel = new LoginViewModel();
-        userViewModel = new UserViewModel();
-        firebaseAuth = FirebaseAuth.getInstance();
-        firebaseUser = firebaseAuth.getCurrentUser();
-    }
-    private void setListener(View view) {
+//    private void initialize(View view) {
+//        ivProfile = view.findViewById(R.id.ivProfilePicture);
+//        gso = GoogleService.getGso(view.getContext());
+//        gsc = GoogleService.getGsc(view.getContext());
+//        loginViewModel = new LoginViewModel();
+//        userViewModel = new UserViewModel();
+//        firebaseAuth = FirebaseAuth.getInstance();
+//        firebaseUser = firebaseAuth.getCurrentUser();
+//    }
+//    private void setListener(View view) {
+//
+//        btnLogout.setOnClickListener(e -> {
+//            gsc.signOut().addOnCompleteListener(requireActivity(), new OnCompleteListener<Void>() {
+//                @Override
+//                public void onComplete(@NonNull Task<Void> task) {
+//                    loginViewModel.googleLogout();
+//                    Toast.makeText(view.getContext(), "Signed Out", Toast.LENGTH_SHORT).show();
+//                }
+//            });
+//            ActivityChanger.changeActivity(view.getContext(), LoginActivity.class);
+//        });
+//    }
 
-        btnLogout.setOnClickListener(e -> {
-            gsc.signOut().addOnCompleteListener(requireActivity(), new OnCompleteListener<Void>() {
-                @Override
-                public void onComplete(@NonNull Task<Void> task) {
-                    loginViewModel.googleLogout();
-                    Toast.makeText(view.getContext(), "Signed Out", Toast.LENGTH_SHORT).show();
-                }
-            });
-            ActivityChanger.changeActivity(view.getContext(), LoginActivity.class);
-        });
-    }
-
-    private void setProfilePicture(View view) {
-        ImageView imageView = view.findViewById(R.id.ivProfile);
+//    private void setProfilePicture(View view) {
+//        ImageView imageView = view.findViewById(R.id.ivProfilePicture);
 //        Glide.with(view.getContext())
 //                .load(user.getProfile())
 //                .into(imageView);
 //        String imageUrl = "https://firebasestorage.googleapis.com/v0/b/nitip-6f389.appspot.com/o/profile_pictures%2Fsuperxyber123%40gmail.com?alt=media&token=522db4c1-2ac6-45a2-8c0d-7b1b71a0950f";
-        String imageUrl = this.user.getProfile();
-        new DownloadImageTask(imageView).execute(imageUrl);
-    }
+//        String imageUrl = this.user.getProfile();
+//        new DownloadImageTask(imageView).execute(imageUrl);
+//    }
 }
