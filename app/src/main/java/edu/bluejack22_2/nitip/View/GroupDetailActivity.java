d9d678b0150e790166bed7e9722edc5c10d8ace1
Kevin Christian <kdotchrist30@gmail.com>
2023-05-08 16:04:33 +0700

    Added Titip Button to Group Detail Page

--- app/src/main/java/edu/bluejack22_2/nitip/View/GroupDetailActivity.java
+++ app/src/main/java/edu/bluejack22_2/nitip/View/GroupDetailActivity.java
@@ -2,14 +2,18 @@ package edu.bluejack22_2.nitip.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 import edu.bluejack22_2.nitip.R;
 
 public class GroupDetailActivity extends AppCompatActivity {
 
     private Button btnBack;
+
+    private TextView tvGroupName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,15 +22,22 @@ public class GroupDetailActivity extends AppCompatActivity {
 
         initialize();
         setListener();
+        setValues();
     }
 
     private void initialize() {
         btnBack = findViewById(R.id.btnBack);
+        tvGroupName = findViewById(R.id.tvGroupName);
     }
 
     private void setListener() {
         btnBack.setOnClickListener(e -> {
             finish();
         });
+    }
+
+    private void setValues() {
+        Bundle extras = getIntent().getExtras();
+        tvGroupName.setText(extras.getString("GroupName"));
     }
 }
